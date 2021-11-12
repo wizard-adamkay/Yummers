6bafb28f06284baeb886abf692a015b2edcabfcd
wizard-adamkay <37917852+WIZARDOFGAPE@users.noreply.github.com>
2021-11-12 02:54:21 -0800

    added users with email authentication

--- app/src/main/java/com/example/yummers/MainActivity.java
+++ app/src/main/java/com/example/yummers/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.yummers;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -30,38 +31,40 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void test(View view) {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
         // Create a new user with a first and last name
-        Map<String, Object> user = new HashMap<>();
-        user.put("first", "Ada");
-        user.put("last", "Lovelace");
-        user.put("born", 1815);
-        db.collection("users")
-                .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d("yes", "DocumentSnapshot added with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w("yes", "Error adding document", e);
-                    }
-                });
-        db.collection("users")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.d("yes", document.getId() + " => " + document.getData());
-                            }
-                        } else {
-                            Log.w("yes", "Error getting documents.", task.getException());
-                        }
-                    }
-                });
+//        Map<String, Object> user = new HashMap<>();
+//        user.put("first", "Ada");
+//        user.put("last", "Lovelace");
+//        user.put("born", 1815);
+//        db.collection("users")
+//                .add(user)
+//                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//                    @Override
+//                    public void onSuccess(DocumentReference documentReference) {
+//                        Log.d("yes", "DocumentSnapshot added with ID: " + documentReference.getId());
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Log.w("yes", "Error adding document", e);
+//                    }
+//                });
+//        db.collection("users")
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            for (QueryDocumentSnapshot document : task.getResult()) {
+//                                Log.d("yes", document.getId() + " => " + document.getData());
+//                            }
+//                        } else {
+//                            Log.w("yes", "Error getting documents.", task.getException());
+//                        }
+//                    }
+//                });
     }
 }
