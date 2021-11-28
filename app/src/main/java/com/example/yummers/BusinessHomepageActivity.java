dc143144774c79d0199cfa1e592dad94a1d52e92
Kris Chan <bigmidget489@gmail.com>
2021-11-27 18:17:58 -0800

    started order history

--- app/src/main/java/com/example/yummers/BusinessHomepageActivity.java
+++ app/src/main/java/com/example/yummers/BusinessHomepageActivity.java
@@ -12,18 +12,19 @@ import android.widget.TextView;
 import com.example.yummers.models.Business;
 
 public class BusinessHomepageActivity extends AppCompatActivity {
-
+    Business business;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_business_homepage);
         TextView title = findViewById(R.id.textView4);
-        Business business = (Business) getIntent().getSerializableExtra("business");
+        business = (Business) getIntent().getSerializableExtra("business");
         Log.e("business", business.toString());
         title.setText(business.getName());
     }
     public void pastOrders(View view){
-
+        Intent intent = new Intent(this, PastOrdersActivity.class);
+        intent.putExtra("restaurantID", business.getID());
     }
     public void currentOrders(View view){
 
